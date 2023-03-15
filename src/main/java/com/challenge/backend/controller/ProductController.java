d2005454dcde46a64d5b4a0391fdc0252b315744
diegorichi <diegorichi@gmail.com>
2023-03-15 17:07:06 -0300

    Sonar fixes

--- src/main/java/com/challenge/backend/controller/ProductController.java
+++ src/main/java/com/challenge/backend/controller/ProductController.java
@@ -1,15 +1,16 @@
 package com.challenge.backend.controller;
 
-import com.challenge.backend.domain.Product;
 import com.challenge.backend.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
 import javax.validation.constraints.Positive;
-import java.util.List;
 
 @RestController
 @RequestMapping("/product")
@@ -25,9 +26,7 @@ public class ProductController {
     @GetMapping("/{id}/similar")
     public Mono<ResponseEntity<?>> getSimilarProducts(@PathVariable @Positive long id) {
 
-        Mono<List<Product>> fluxP =  productService.getSimilarProducts(id).collectList();
-
-        return fluxP
+        return productService.getSimilarProducts(id).collectList()
                 .flatMap(list -> !list.isEmpty()
                         ? Mono.just(ResponseEntity.ok(list))
                         : Mono.just(ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found"))
